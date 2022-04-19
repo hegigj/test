d840291c1c09ab2f7840a20fe7b9527118422152
hgjoka <hegi.gjoka@eng.it>
2022-04-19 19:08:59 +0200

    shtova nje consol log

---
+++ src/app/event-create-edit-modal/event-create-edit-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-create-edit-modal',
+  templateUrl: './event-create-edit-modal.component.html',
+  styleUrls: ['./event-create-edit-modal.component.scss']
+})
+export class EventCreateEditModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
